c358f74299b26cae62f67cce01bd5420118ceaef
mannaKim <gkssk2286@naver.com>
2022-08-11 10:07:25 +0900

    first commit

---
+++ src/main/java/com/ezen/m16/SpringBootMaven16ShopApplication.java
@@ -0,0 +1,13 @@
+package com.ezen.m16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMaven16ShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMaven16ShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ezen/m16/SpringBootMaven16ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezen.m16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMaven16ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
